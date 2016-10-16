fc156de23a44ad5009d96ff2eafe53842de6c006
naveen2911 <motwani.naveen61@gmail.com>
2016-10-16 16:38:43 +0530

    Create sequential wprlf;pw

---
+++ NumberGuessWorkflowHost/WorkflowHostForm.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace NumberGuessWorkflowHost
+{
+    partial class WorkflowHostForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.NewGame = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // NewGame
+            // 
+            this.NewGame.Location = new System.Drawing.Point(109, 84);
+            this.NewGame.Name = "NewGame";
+            this.NewGame.Size = new System.Drawing.Size(75, 23);
+            this.NewGame.TabIndex = 0;
+            this.NewGame.Text = "button1";
+            this.NewGame.UseVisualStyleBackColor = true;
+            // 
+            // WorkflowHostForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(384, 381);
+            this.Controls.Add(this.NewGame);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "WorkflowHostForm";
+            this.Text = "WorkflowHostForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button NewGame;
+    }
+}
